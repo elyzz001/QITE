64cb0842e0ca10f7676da94ef149bf182844a66a
elyzz001 <45132412+elyzz001@users.noreply.github.com>
2025-01-22 22:33:30 -0600

    Update binary_functions.py
    Fixed print statement

--- code_v4/binary_functions.py
+++ code_v4/binary_functions.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
   for n0 in range(b**nbit):
    assert(n0==Bas2Int(Int2Bas(n0,b,nbit),b))
    if(nbit==4):
-    print n0,Int2Bas(n0,b,nbit)
+    print(n0,Int2Bas(n0,b,nbit))
   t1 = time.time()
   dt = t1-t0
   v.append(dt)
